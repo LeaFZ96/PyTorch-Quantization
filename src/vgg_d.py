37af76f5093b6a12c9facb33e907d72fb3022683
LeaFZ <leafzhang714@gmail.com>
2019-07-15 17:55:10 -0500

    add vgg model & log dir

--- src/vgg_d.py
+++ src/vgg_d.py
@@ -1,12 +1,17 @@
 import torch
-import torchvision
-import torchvision.transforms as transforms
-import numpy as np
-import matplotlib.pyplot as plt
 import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torch.distributed as dist
+import numpy as np
 import torch.nn.functional as F
 import torch.optim as optim
 import time
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+dist.init_process_group(backend="nccl")
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -14,72 +19,78 @@ transform = transforms.Compose(
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=80,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=1600,
                                           shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=80,
+testloader = torch.utils.data.DataLoader(testset, batch_size=100,
                                          shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
+net = models.vgg16()
+net.cuda()
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(10, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 64)
-        self.fc3 = nn.Linear(64, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-net = Net()
-net.to(device)
+net = torch.nn.parallel.DistributedDataParallel(net)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.003, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.8)
 
-last_time = time.time()
-start_time = last_time
+start_time = time.time()
+last_time = start_time
 print("start train")
 
 for epoch in range(10):  # loop over the dataset multiple times
 
     running_loss = 0.0
+    read_time = 0.0
+    zero_time = 0.0
+    forward_time = 0.0
+    backward_time = 0.0
+    optimize_time = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data[0].to(device), data[1].to(device)
+        time_0 = time.time()
+        inputs, labels = data[0].cuda(), data[1].cuda()
 
         # zero the parameter gradients
+        time_x = time.time()
         optimizer.zero_grad()
 
+
         # forward + backward + optimize
+        time_1 = time.time()
         outputs = net(inputs)
+
+        time_2 = time.time()
         loss = criterion(outputs, labels)
         loss.backward()
+
+        time_3 = time.time()
         optimizer.step()
+
+        time_4 = time.time()
+
+        read_time += time_x - time_0
+        zero_time += time_1 - time_x
+        forward_time += time_2 - time_1
+        backward_time += time_3 - time_2
+        optimize_time += time_4 - time_3
 
         # print statistics
         running_loss += loss.item()
-        if i % 100 == 99:    # print every 20 mini-batches
-            print('epoch: %d\t iter: %d\t loss: %.3f\t time used: %.3f\t' %
-                  (epoch + 1, i + 1, running_loss / 100, time.time() - last_time))
+        if i % 5 == 4:    # print every 2000 mini-batches
+            print('epoch: %d\t iter: %5d\t loss: %.3f\t total_t: %.3f\t read_t: %.4f\t zero_t: %.4f\t forward_t: %.4f\t backward_t: %.4f\t optimize_t: %.4f\t' %
+                  (epoch + 1, i + 1, running_loss / 5, time.time() - last_time, read_time, zero_time, forward_time, backward_time, optimize_time))
             last_time = time.time()
             running_loss = 0.0
+            read_time = 0.0
+            zero_time = 0.0
+            forward_time = 0.0
+            backward_time = 0.0
+            optimize_time = 0.0
 
 print('total time: %.3f' % (time.time() - start_time))
 print('Finished Training')
--- src/vgg_s.py
+++ src/vgg_s.py
@@ -1,12 +1,15 @@
 import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 import torchvision
+import torchvision.models as models
 import torchvision.transforms as transforms
 import numpy as np
-import matplotlib.pyplot as plt
-import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import time
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -14,7 +17,7 @@ transform = transforms.Compose(
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=80,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=400,
                                           shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
@@ -25,44 +28,23 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=80,
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
+net = models.vgg16()
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(10, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 64)
-        self.fc3 = nn.Linear(64, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-net = Net()
 net.to(device)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.003, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=0.0025, momentum=0.8)
 
-last_time = time.time()
-start_time = last_time
+start_time = time.time()
+last_time = start_time
 print("start train")
 
-for epoch in range(10):  # loop over the dataset multiple times
+for epoch in range(40):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data[0].to(device), data[1].to(device)
+        inputs, labels = data[0].cuda(), data[1].cuda()
 
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -75,9 +57,9 @@ for epoch in range(10):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 100 == 99:    # print every 20 mini-batches
-            print('epoch: %d\t iter: %d\t loss: %.3f\t time used: %.3f\t' %
-                  (epoch + 1, i + 1, running_loss / 100, time.time() - last_time))
+        if i % 20 == 19:    # print every 2000 mini-batches
+            print('epoch: %d\t iter: %5d\t loss: %.3f\t time used: %.3f\t' %
+                  (epoch + 1, i + 1, running_loss / 20, time.time() - last_time))
             last_time = time.time()
             running_loss = 0.0
 
